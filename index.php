c8a15df9ab50b8e18820d1416e6f77dc82a44353
JackNel <jcneligan@gmail.com>
2018-05-28 22:35:33 -0400

    Check if prime implemented, need to fix duplicate printing

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
-  
+  if($_GET) {
+    $i = 2;
+    while($i < $_GET["number"]) {
+      if($_GET["number"] % $i == 0) {
+        echo $_GET["number"]." is divisible by $i";
+      }
+      $i++;
+    }
+  }
 ?>
 
 <p>Please enter a whole number</p>
