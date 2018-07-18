2859b92b16443b3c42fb8997676182c85c6efd7e
JackNel <jcneligan@gmail.com>
2018-07-17 23:14:20 -0400

    add some styling

---
+++ primeTest.php
@@ -0,0 +1,21 @@
+<?php
+if (is_numeric($_GET["number"]) && $_GET["number"] > 0 && $_GET["number"] == round($_GET["number"], 0)) {
+    $i = 2;
+    $isPrime = true;
+
+    while ($i < $_GET["number"]) {
+        if ($_GET["number"] % $i == 0) {
+            $isPrime = false;
+        }
+        $i++;
+    }
+
+    if ($isPrime) {
+        echo "<p>$i is a prime number!</p>";
+    } else {
+        echo "<p>$i is not prime.</p>";
+    }
+} else if ($_GET) {
+    echo "<p>Please enter a positive whole number</p>";
+}
+?>
